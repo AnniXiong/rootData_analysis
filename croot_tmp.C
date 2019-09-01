acfda852ad2b6709fa08569985bdd96703c13461
Anni Xiong <annixiong23@gmail.com>
2019-09-01 19:30:13 +0800

    count number of jets above some E threshold

--- croot_tmp.C
+++ croot_tmp.C
@@ -37,18 +37,18 @@ void croot_tmp::Loop()
    if (fChain == 0) return;
 
    Long64_t nentries = fChain->GetEntriesFast();
-   nentries = 20;
+   //nentries = 200;
 
    Long64_t nbytes = 0, nb = 0;
    
    FILE *fout_pt = fopen("output/ZvvHbb_AntiKt4LCTopoJets_pt.dat", "w");
    FILE *fout_n = fopen("output/ZvvHbb_AntiKt4LCTopoJets_n.dat", "w");
-   TH1F *jet_n = new TH1F("Njet","",50,0,100);
-   TH1F *jet_n_L20 = new TH1F("Njet_L20", "",50,0,100);
-   TH1F *jet_n_L50 = new TH1F("Njet_L50", "",50, 0,100);
+   TH1F *jet_n = new TH1F("Njet","",50,0,70);
+   TH1F *jet_n_L20 = new TH1F("Njet_L20", "",50,0,70);
+   TH1F *jet_n_L50 = new TH1F("Njet_L50", "",50, 0,70);
    
    
-   for (Long64_t jentry=0; jentry<nentries;jentry++) {
+   for (Long64_t jentry=0; jentry<nentries; jentry++) {
       Long64_t ientry = LoadTree(jentry);
       if (ientry < 0) break;
       nb = fChain->GetEntry(jentry);   
@@ -61,21 +61,37 @@ void croot_tmp::Loop()
       jet_n ->Fill (AntiKt4LCTopoJets_n);
       fprintf (fout_n, "%i ", AntiKt4LCTopoJets_n);
       
-      
       std:: vector<float> &ptr = *AntiKt4LCTopoJets_pt;
       
+        
+      // Fill jet_number_L histograms if the event has at least one jet with pt larger than 30/50 GeV
+      int L30 = 0;
+      int L50 = 0;
+      
       for (int i =0; i< AntiKt4LCTopoJets_n; i++) {
-      	std:: cout <<"m "<<ptr[i] << ",  ";
+      	std:: cout <<"pt" <<ptr[i] << ",  ";
       	fprintf(fout_pt, "%i ",(int)ptr[i]);
+      	
+      	if ((int)ptr[i] > 30000) L30+=1;
+      	if ((int)ptr[i] > 50000) L50+=1;
       }
       
+      if (L30 > 0) jet_n_L20 ->Fill (L30);
+      if (L50 > 0) jet_n_L50 ->Fill (L50);
       
-        fprintf(fout_pt, "\n");
-        std::cout << ""<<endl;
+      cout <<"" <<endl;
+      cout << ": L30 "<< L30 << " L50 " << L50 << ",  Out of total number " << AntiKt4LCTopoJets_n << endl;
+      L30 = 0 ; L50 = 0;
+      
+      fprintf(fout_pt, "\n");
+      std::cout << ""<<endl;
+      
    }
     
 	TFile *f = new TFile ("output/plot.root", "recreate");
 	jet_n ->Write ();
+	jet_n_L20 ->Write ();
+	jet_n_L50 ->Write ();
 	f-> Close();
 
 }
